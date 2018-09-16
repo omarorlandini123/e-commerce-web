2e49ff02f188a80e5d3d1a484d5fdd5ca1b11fde
Omar Orlandini <omarorlandini123@gmail.com>
2018-09-15 21:37:06 -0500

    valida codigo sms listo

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -19,11 +19,10 @@ class TokenController extends Controller
 
         $token_var = new Token;
         $token_var->token_numero = $numero;
-        $token_var->token_codigo_sms = intval( "".rand(1,9)  . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) );        
+        $token_var->token_codigo_sms = "".intval( "".rand(1,9)  . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) );        
         $token_var->token = md5(uniqid(rand(), true)) ;
         $token_var->token_fecha_creacion = Carbon::now();
         $token_var->save();
-        return $token_var;
         
         if($token_var->token_id>0){
             $token_rpta = new Token;
