bb070f57abe36fafae3ebd0e9f00a66f98d79820
Omar Orlandini <omarorlandini123@gmail.com>
2018-09-15 21:33:50 -0500

    valida codigo sms listo

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -19,7 +19,7 @@ class TokenController extends Controller
 
         $token_var = new Token;
         $token_var->token_numero = $numero;
-        $token_var->token_codigo_sms = rand(100000, 999999).'';        
+        $token_var->token_codigo_sms = intval( "".rand(1,9)  . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) );        
         $token_var->token = md5(uniqid(rand(), true)) ;
         $token_var->token_fecha_creacion = Carbon::now();
         $token_var->save();
@@ -29,13 +29,17 @@ class TokenController extends Controller
             $token_rpta->token_codigo_sms=true;
             return $token_rpta;
         }
-        return null;
+
+        $error = new Error;
+        $error->codigo = "E_0002";
+        $error->descripcion = "No se ha podido generar el código";
+        return $error;
     }
 
     public function solicitaRegistro(Request $request, $codigo){
         $error = new Error;
         if($codigo==null){            
-            $error->codigo = "E_0002";
+            $error->codigo = "E_0003";
             $error->descripcion = "El codigo a validar está vacío";            
             return $error;
         }
@@ -45,7 +49,7 @@ class TokenController extends Controller
 
         $token_encontrado = Token::where('token_codigo_sms', '=', $codigo)->orderBy('token_fecha_creacion','desc')->take(1)->get();
         if($token_encontrado==null || count($token_encontrado)==0 || $token_encontrado[0]==null){           
-            $error->codigo = "E_0003";
+            $error->codigo = "E_0004";
             $error->descripcion = "El codigo no se ha encontrado";            
             return $error;
         }
