81ac78f63a152adf2475b99aed21fc714d142ded
Omar Hans Orlandini Vera <omar.orlandini@viettelperu.com>
2018-09-17 11:43:21 -0500

    Cantidad validada

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -41,7 +41,8 @@ class TokenController extends Controller
 
         $token_encontrado = Token::where([['token_codigo_sms', '=', $codigo],['token_numero', '=', $numero]])->orderBy('token_fecha_creacion','desc')->take(1)->get();
         $cantidad=$token_encontrado->count();
-        if($token_encontrado==null || $cantidad ==0|| count($token_encontrado)==0 || $token_encontrado[0]==null){           
+        var_dump($cantidad);
+        if($token_encontrado==null || $cantidad == 0 || count($token_encontrado)==0 || $token_encontrado[0]==null){           
             return new Error("E_0004","El codigo no se ha encontrado");
         }
 
