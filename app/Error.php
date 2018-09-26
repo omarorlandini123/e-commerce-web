2c35c571df444166a1210811b83cd9cbf042a2f3
Omar Orlandini <omarorlandini123@gmail.com>
2018-09-26 15:07:46 -0500

    registrar empresa

--- app/Error.php
+++ app/Error.php
@@ -29,33 +29,33 @@ class Error
     static function getError($E_code){
         switch($E_code){
             case 1:
-                return new Error($this::getErrorCode($E_code),"El numero a enviar está vacío");
+                return new Error(Error::getErrorCode($E_code),"El numero a enviar está vacío");
             case 2:
-                return new Error($this::getErrorCode($E_code),"No se ha podido generar el código");
+                return new Error(Error::getErrorCode($E_code),"No se ha podido generar el código");
             case 3:
-                return new Error($this::getErrorCode($E_code),"El código a validar está vacío");
+                return new Error(Error::getErrorCode($E_code),"El código a validar está vacío");
             case 4:
-                return new Error($this::getErrorCode($E_code),"El código no se ha encontrado");
+                return new Error(Error::getErrorCode($E_code),"El código no se ha encontrado");
             case 5:
-                return new Error($this::getErrorCode($E_code),"El token ingresado es inválido");
+                return new Error(Error::getErrorCode($E_code),"El token ingresado es inválido");
             case 6:
-                return new Error($this::getErrorCode($E_code),"Necesita llenar los valores completamente");
+                return new Error(Error::getErrorCode($E_code),"Necesita llenar los valores completamente");
             case 7:
-                return new Error($this::getErrorCode($E_code),"No se pudo guardar el usuario");
+                return new Error(Error::getErrorCode($E_code),"No se pudo guardar el usuario");
             case 8:
-                return new Error($this::getErrorCode($E_code),"El DNI ya se encuentra registrado");
+                return new Error(Error::getErrorCode($E_code),"El DNI ya se encuentra registrado");
             case 9:
-                return new Error($this::getErrorCode($E_code),"No se ha encontrado un usuario asociado");
+                return new Error(Error::getErrorCode($E_code),"No se ha encontrado un usuario asociado");
             case 10:
-                return new Error($this::getErrorCode($E_code), "No se pudo guardar la empresa");
+                return new Error(Error::getErrorCode($E_code), "No se pudo guardar la empresa");
             case 11:
-                return new Error($this::getErrorCode($E_code), "No se encuentra el código de empresa");
+                return new Error(Error::getErrorCode($E_code), "No se encuentra el código de empresa");
             case 12:
-                return new Error($this::getErrorCode($E_code), "Debe ingresar el RUC de su empresa");
+                return new Error(Error::getErrorCode($E_code), "Debe ingresar el RUC de su empresa");
             case 13:
-                return new Error($this::getErrorCode($E_code), "El RUC ya se encuentra registrado");
+                return new Error(Error::getErrorCode($E_code), "El RUC ya se encuentra registrado");
             case 14:
-                return new Error($this::getErrorCode($E_code), "No se ha encontrado el RUC de su empresa");
+                return new Error(Error::getErrorCode($E_code), "No se ha encontrado el RUC de su empresa");
         }
     }
 
