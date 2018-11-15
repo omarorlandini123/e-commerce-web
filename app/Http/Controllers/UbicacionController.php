a95e8086cf1c533c7a57a1cd9f790113f3e77938
Omar Orlandini <omarorlandini123@gmail.com>
2018-11-14 20:27:44 -0500

    Ubicaciones listar fix vacio

--- app/Http/Controllers/UbicacionController.php
+++ app/Http/Controllers/UbicacionController.php
@@ -16,30 +16,7 @@ class UbicacionController extends Controller
     {
         $rpta = new Respuesta;
 
-        $token_var = Token::where('token', $token)->first();
-
-        if ($token_var == null || count($token_var) == 0) {
-            $contenidoError = Error::getError(5);
-            $rpta->tieneError = true;
-            $rpta->error = $contenidoError;
-            return $rpta;
-        }
-
-        $tokenUsuarioFound = TokenUsuario::where('token_token_id', $token_var->token_id)->first();
-        if ($tokenUsuarioFound == null) {
-            $contenidoError = Error::getError(9);
-            $rpta->tieneError = true;
-            $rpta->error = $contenidoError;
-            return $rpta;
-        }
-
-        $this->usuariofind = Usuario::where('usuario_id', $tokenUsuarioFound->usuario_usuario_id)->first();
-        if ($this->usuariofind == null) {
-            $contenidoError = Error::getError(9);
-            $rpta->tieneError = true;
-            $rpta->error = $contenidoError;
-            return $rpta;
-        }
+        
 
         $ubicaciones = Ubicaciones::where('ubicacion','like',"%Lima%")->get();
 
