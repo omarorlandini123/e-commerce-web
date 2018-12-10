21dac1cd56d36d462834172139903f2dce58e49d
Omar Orlandini <omarorlandini123@gmail.com>
2018-12-10 10:28:53 -0500

    add web empresa preview

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::post('/Empresa/Actualizar/{idEmpresa}/{token}','EmpresaController@actuali
 Route::get('/Empresa/Listar/{token}/{condicion}','EmpresaController@listar');
 Route::get('/Empresa/Eliminar/{idEmpresa}/{token}','EmpresaController@eliminar');
 Route::get('/Empresa/Obtener/{idEmpresa}/{token}','EmpresaController@obtener');
+Route::get('/Empresa/Preview/{idEmpresa}/{token}','ProductoController@getPreview');
 
 Route::get('/Ubicacion/Listar/{token}','UbicacionController@listarUbicaciones');
 
