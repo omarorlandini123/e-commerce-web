f0517ee11d1dec97846d13a44d6c97aade76b035
Omar Orlandini <omarorlandini123@gmail.com>
2018-12-21 12:01:32 -0500

    Pedido upd

--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -8,16 +8,7 @@
         method="post">
         @csrf
         <div class="row">
-            @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            
-            @endif
+           
 
             <div class="col-sm-12">
                 <div class="form-group">
