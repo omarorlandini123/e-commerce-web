7775a72e026f5658ce756434079b5504152c99f0
Omar Orlandini <omarorlandini123@gmail.com>
2018-12-26 10:11:24 -0500

    afiche preview

--- resources/views/afiches/index.blade.php
+++ resources/views/afiches/index.blade.php
@@ -15,25 +15,58 @@
     </div>
     <div class="row">
         <div class="col-sm-12">
-            <div class="card" style="margin-bottom: 10px;">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-3">
-                            <center> <img src="http://www.sidrafanjul.com/imagenes/producto-ribanora-g.jpg" width="50" height="50"
-                                alt="Cinque Terre" class="rounded-circle"></center>
-                        </div>
-                        <div class="col-9" >
-                            <h5 class="card-title">Card title</h5>
-                            <div class="form-group">
-                                
-                                <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="¿Cuántos quieres?">
-                                <small id="emailHelp" class="form-text text-muted">Coloca la cantidad que deseas</small>
+            @if($afiche->afiche_detalle!=null && count($afiche->afiche_detalle)>0)
+                @foreach ($afiche->grupo_afiche as $grupo)
+                    <h3>{{$grupo->nombre}}</h3>
+                     @foreach ($grupo->afiche_detalle as $detalle)
+                        <div class="card" style="margin-bottom: 10px;">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-3">
+                                        <center> <img src="/Producto/Preview/{{$detalle->producto->producto_id}}/{{$token}}" width="50" height="50"
+                                            alt="{{$detalle->producto->producto_nombre}}" class="rounded-circle"></center>
+                                    </div>
+                                    <div class="col-9" >
+                                        <h5 class="card-title">{{$detalle->producto->producto_nombre}}</h5>
+                                        <div class="form-group">                                
+                                            <input type="number" class="form-control" name="prod:{{$detalle->producto->producto_id}}" id="prod:{{$detalle->producto->producto_id}}" aria-describedby="cantidad" placeholder="¿Cuántos quieres?">
+                                            <small id="cantidad" class="form-text text-muted">Coloca la cantidad que deseas</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                    @endforeach 
+                    <hr>
+                @endforeach
+
+                @if(count($afiche->grupo_afiche)>0 && count($afiche->detalle_sin_grupo())>0)
+                    <h3>Productos adicionales</h3>
+                @endif
+
+                @foreach ($afiche->detalle_sin_grupo() as $detalle)
+                    <div class="card" style="margin-bottom: 10px;">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-3">
+                                    <center> <img src="/Producto/Preview/{{$detalle->producto->producto_id}}/{{$token}}" width="50" height="50"
+                                        alt="{{$detalle->producto->producto_nombre}}" class="rounded-circle"></center>
+                                </div>
+                                <div class="col-9" >
+                                    <h5 class="card-title">{{$detalle->producto->producto_nombre}}</h5>
+                                    <div class="form-group">                                
+                                        <input type="number" class="form-control" name="prod:{{$detalle->producto->producto_id}}" id="prod:{{$detalle->producto->producto_id}}" aria-describedby="cantidad" placeholder="¿Cuántos quieres?">
+                                        <small id="cantidad" class="form-text text-muted">Coloca la cantidad que deseas</small>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            
+                    </div>    
+                @endforeach
+                          
+            @else
+            <h3>Este afiche no tiene productos :(</h3>
+            @endif
         </div>
     </div>
 
