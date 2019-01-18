9baf8f2a8b6a74fcb865cdc2752a5855c742e990
Omar Orlandini <omarorlandini123@gmail.com>
2019-01-18 12:07:58 -0500

    listar pedidos de afiche tercero

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -277,42 +277,31 @@ class PedidoController extends Controller
         }
         $this->usuario_id = $usuariofind->usuario_id;
 
-        $productos = Producto::whereHas('detalle_pedido', function ($a) {
-            $a->whereHas('pedido', function ($b) {
-                $b->whereHas('freeler', function ($c) {
-                    $c->where('usuario_id', $this->usuario_id);
-                });
-            });
+        $afiches = Afiche::whereHas('pedido', function ($b) {
+            $b->whereHas('freeler', function ($c) {
+            })->WhereNotNull('afiche_id');
         })->whereHas('empresa', function ($b) {
             $b->whereHas('freeler', function ($c) {
                 $c->where('usuario_id', '!=', $this->usuario_id);
             });
         })
-            ->with(['detalle_pedido', 'detalle_pedido.pedido', 'detalle_pedido.producto', 'producto_detalle', 'detalle_pedido.pedido.freeler', 'detalle_pedido.pedido.freeler.usuario'])
-            ->get();
+        ->with([
+            'pedido.detalle_pedido', 
+            'pedido.detalle_pedido', 
+            'pedido.detalle_pedido.producto', 
+            'pedido.detalle_pedido.producto.producto_detalle', 
+            'pedido.detalle_pedido.producto.producto_detalle.item_almacen', 
+            'pedido.detalle_pedido.producto.producto_detalle.item_almacen.almacen', 
+            'pedido.detalle_pedido.producto.producto_detalle.item_almacen.almacen.empresa', 
+            'pedido.freeler', 
+            'pedido.freeler.usuario',
+            'empresa'])
+        ->get();
 
-        for ($i = 0; $i < count($productos); $i++) {
-            $detalles = $productos[$i]->producto_detalle;
-            for ($r = 0; $r < count($detalles); $r++) {
-                $detalles[$r]->item_almacen = ItemAlmacen::where('item_almacen_id', $detalles[$r]->item_almacen_id)->first();
-                $detalles[$r]->item_almacen->almacen = Almacen::where('almacen_id', $detalles[$r]->item_almacen->almacen_id)->first();
-                $detalles[$r]->item_almacen->almacen->empresa = Empresa::where('empresa_id', $detalles[$r]->item_almacen->almacen->empresa_id)->first();
-            }
+  
+        return $afiches;
 
-            $path = null;
-            if ($productos[$i]->preview_img == null) {
-                if (count($productos[$i]->producto_detalle) == 1) {
-                    $path = $productos[$i]->producto_detalle[0]->item_almacen->preview_img;
-                }
-            } else {
-                $path = $productos[$i]->preview_img;
-            }
-
-            $productos[$i]->preview_img = $path;
-
-        }
-
-        return $productos;
+     
 
     }
 
