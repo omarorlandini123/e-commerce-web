66b48ab295a5ce9b42035f180293a428d5d14adc
Omar Orlandini <omarorlandini123@gmail.com>
2019-01-27 22:16:24 -0500

    pedibles fix 5

--- app/Pedible.php
+++ app/Pedible.php
@@ -11,11 +11,11 @@ class Pedible extends Model
 
     public function producto()
     {        
-        return $this->belongsTo('App\Producto',  'pedible_id','producto_id')->where('pedibles.tipo_pedible',1);        
+        return $this->belongsTo('App\Producto',  'pedible_id','producto_id');      
     }
 
     public function afiche()
     {
-        return $this->belongsTo('App\Afiche', 'pedible_id','afiche_id')->where('pedibles.tipo_pedible',2);  
+        return $this->belongsTo('App\Afiche', 'pedible_id','afiche_id');
     }
 }
