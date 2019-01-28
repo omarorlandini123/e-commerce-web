eae14d86f3da9b10eb8d74ec8d6a9f9b4c9743ef
Omar Orlandini <omarorlandini123@gmail.com>
2019-01-27 19:48:29 -0500

    pedibles

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -13,6 +13,7 @@ use App\Token;
 use App\TokenUsuario;
 use App\Usuario;
 use App\Afiche;
+use App\Pedible;
 use Illuminate\Http\Request;
 
 class PedidoController extends Controller
@@ -64,41 +65,45 @@ class PedidoController extends Controller
             return $rpta;
         }
 
+        $pedibles = Pedible::where('freeler_shared_id',$freelerFind->freeler_id)
+        ->where('freeler_id',$freelerFind->freeler_id)
+        ->get();
+        return $pedibles;
 
-        $productos = Producto::whereHas('detalle_pedido', function ($a) {
-            $a->whereHas('pedido', function ($b) {
-                $b->whereHas('freeler', function ($c) {
-                })->WhereNull('afiche_id')
-                ->where('eliminado',0);
-            });
-        })->whereHas('empresa', function ($b) {
-            $b->whereHas('freeler', function ($c) {
-                $c->where('usuario_id', $this->usuario_id);
-            });
-        })
-            ->with(['detalle_pedido', 'detalle_pedido.pedido', 'detalle_pedido.producto', 'producto_detalle', 'detalle_pedido.pedido.freeler', 'detalle_pedido.pedido.freeler.usuario'])
-            ->get();
+        // $productos = Producto::whereHas('detalle_pedido', function ($a) {
+        //     $a->whereHas('pedido', function ($b) {
+        //         $b->whereHas('freeler', function ($c) {
+        //         })->WhereNull('afiche_id')
+        //         ->where('eliminado',0);
+        //     });
+        // })->whereHas('empresa', function ($b) {
+        //     $b->whereHas('freeler', function ($c) {
+        //         $c->where('usuario_id', $this->usuario_id);
+        //     });
+        // })
+        //     ->with(['detalle_pedido', 'detalle_pedido.pedido', 'detalle_pedido.producto', 'producto_detalle', 'detalle_pedido.pedido.freeler', 'detalle_pedido.pedido.freeler.usuario'])
+        //     ->get();
 
-        for ($i = 0; $i < count($productos); $i++) {
-            $detalles = $productos[$i]->producto_detalle;
-            for ($r = 0; $r < count($detalles); $r++) {
-                $detalles[$r]->item_almacen = ItemAlmacen::where('item_almacen_id', $detalles[$r]->item_almacen_id)->first();
-                $detalles[$r]->item_almacen->almacen = Almacen::where('almacen_id', $detalles[$r]->item_almacen->almacen_id)->first();
-                $detalles[$r]->item_almacen->almacen->empresa = Empresa::where('empresa_id', $detalles[$r]->item_almacen->almacen->empresa_id)->first();
-            }
+        // for ($i = 0; $i < count($productos); $i++) {
+        //     $detalles = $productos[$i]->producto_detalle;
+        //     for ($r = 0; $r < count($detalles); $r++) {
+        //         $detalles[$r]->item_almacen = ItemAlmacen::where('item_almacen_id', $detalles[$r]->item_almacen_id)->first();
+        //         $detalles[$r]->item_almacen->almacen = Almacen::where('almacen_id', $detalles[$r]->item_almacen->almacen_id)->first();
+        //         $detalles[$r]->item_almacen->almacen->empresa = Empresa::where('empresa_id', $detalles[$r]->item_almacen->almacen->empresa_id)->first();
+        //     }
 
-            $path = null;
-            if ($productos[$i]->preview_img == null) {
-                if (count($productos[$i]->producto_detalle) == 1) {
-                    $path = $productos[$i]->producto_detalle[0]->item_almacen->preview_img;
-                }
-            } else {
-                $path = $productos[$i]->preview_img;
-            }
+        //     $path = null;
+        //     if ($productos[$i]->preview_img == null) {
+        //         if (count($productos[$i]->producto_detalle) == 1) {
+        //             $path = $productos[$i]->producto_detalle[0]->item_almacen->preview_img;
+        //         }
+        //     } else {
+        //         $path = $productos[$i]->preview_img;
+        //     }
 
-            $productos[$i]->preview_img = $path;
+        //     $productos[$i]->preview_img = $path;
 
-        }
+        // }
 
         return $productos;
 
