c82022099136a858b0efd6177da53fd51dc25825
Omar Orlandini <omarorlandini123@gmail.com>
2019-01-28 12:11:49 -0500

    pedibles empresa

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -68,7 +68,13 @@ class PedidoController extends Controller
 
         $pedibles = Pedible::where('freeler_shared_id',$freelerFind->freeler_id)
         ->where('freeler_id',$freelerFind->freeler_id)
-        ->with(['producto','afiche','producto.producto_detalle','producto.producto_detalle.item_almacen'])
+        ->with([
+            'producto',
+            'producto.producto_detalle',
+            'producto.producto_detalle.item_almacen',
+            'producto.empresa',
+            'afiche',
+            'afiche.empresa'])        
         ->get();
         return $pedibles;
 
@@ -162,7 +168,13 @@ class PedidoController extends Controller
 
         $pedibles = Pedible::where('freeler_shared_id',$freelerFind->freeler_id)
         ->where('freeler_id','!=',$freelerFind->freeler_id)
-        ->with(['producto','afiche','producto.producto_detalle','producto.producto_detalle.item_almacen'])
+        ->with([
+            'producto',
+            'producto.producto_detalle',
+            'producto.producto_detalle.item_almacen',
+            'producto.empresa',
+            'afiche',
+            'afiche.empresa'])
         ->get();
         return $pedibles;
 
