3ede7a05ec37f52b22b7b5b05eaa9a4b1664cb07
Omar Orlandini <omarorlandini123@gmail.com>
2019-04-14 18:39:19 -0500

    calculo de totales en productos de afiche

--- resources/js/funciones.js
+++ resources/js/funciones.js
@@ -101,13 +101,13 @@ function calcular4(var_id_prodcuto) {
     $('#prec_total_' + var_id_prodcuto).html(
         parseFloat(
             Math.round(
-                $('#prod_'+var_id_prodcuto)
+                $('#prod_'+var_id_prodcuto).val()
                 * parseFloat($('#prec_' + var_id_prodcuto).html()) * 100) / 100).toFixed(2)
 
     );
     $('#prod_cant_' + var_id_prodcuto).html(parseFloat(
         Math.round(
-            $('#prod_'+var_id_prodcuto) * 100) / 100).toFixed(2));
+            $('#prod_'+var_id_prodcuto).val() * 100) / 100).toFixed(2));
     var_total = 0;
 
     $('.total_prod').each(function (index) {
