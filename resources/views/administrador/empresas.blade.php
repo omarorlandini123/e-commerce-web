92c9cd733d61ffa09820744ac82fd1ba7cf5dec5
Omar Orlandini <omarorlandini123@gmail.com>
2019-05-28 20:32:46 -0500

    empresa nombre

--- resources/views/administrador/empresas.blade.php
+++ resources/views/administrador/empresas.blade.php
@@ -5,11 +5,14 @@
 
     <div class="row">
         <div class="col-sm-3">
-        <input class="form-control" type="text" placeholder="Buscar">
+        <input class="form-control" type="text" placeholder="Buscar Empresas">
         <br>
         <div class="list-group">
             @inject('Empresa', 'App\Empresa')
-            @foreach ($Empresa::paginate(8) as $emp)
+            <?
+                $encontrados = $Empresa::paginate(8);
+            ?>
+            @foreach ($encontrados as $emp)
                 
                 <a href="#" class="list-group-item list-group-item-action">{{$emp->empresa_nombre}}</a>
             @endforeach
@@ -17,7 +20,34 @@
         </div>
         </div>
         <div class="col-sm-9">
+            <div class="card">
 
+                <div class="card-body">
+                    <h5 class="card-title">Detalles de la empresa</h5>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <label for="">Nombre</label>
+                            <input class="form-control" type="text" placeholder="Nombre" value="{{$encontrados[0]->empresa_nombre}}">
+                            
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="">Detalle</label>
+                            <input class="form-control" type="text" placeholder="Detalle" value="{{$encontrados[0]->empresa_detalle}}">
+                            
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="">RUC</label>
+                            <input class="form-control" type="text" placeholder="RUC" value="{{$encontrados[0]->empresa_RUC}}">
+                            
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="">Nombre Freeler</label>
+                            <input class="form-control" type="text" placeholder="Freler" disabled value="{{$encontrados[0]->freeler->usuario->nombre_completo()}}">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
