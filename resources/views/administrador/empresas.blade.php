24b8788eac4b628aefdd5706bc309846d6e4581a
Omar Orlandini <omarorlandini123@gmail.com>
2019-05-28 20:19:39 -0500

    empresa nombre

--- resources/views/administrador/empresas.blade.php
+++ resources/views/administrador/empresas.blade.php
@@ -7,7 +7,7 @@
         <div class="col-sm-3">
         <div class="list-group">
             @inject('Empresa', 'App\Empresa')
-            @foreach ($Empresa::all() as $emp)
+            @foreach ($Empresa::paginate(8) as $emp)
                 
                 <a href="#" class="list-group-item list-group-item-action">{{$emp->empresa_nombre}}</a>
             @endforeach
